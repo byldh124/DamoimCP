58893aed4b775525638306252cae20e360271490
byldh124 <byldh124@gmail.com>
2024-11-11 16:58:00 +0900

    signup

--- app/src/main/java/com/moondroid/project01_meetingapp/MyApplication.kt
+++ app/src/main/java/com/moondroid/project01_meetingapp/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.moondroid.project01_meetingapp
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import com.kakao.sdk.common.KakaoSdk
 import com.naver.maps.map.NaverMapSdk
 import dagger.hilt.android.HiltAndroidApp
@@ -11,8 +12,9 @@ class MyApplication : Application() {
         super.onCreate()
         // 카카오 sdk 초기화
         KakaoSdk.init(this, getString(R.string.kakao_client_id))
-
         // 네이버 맵 클라이언트 초기화
         NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient(getString(R.string.naver_client_id))
+        // Firebase 초기화
+        FirebaseApp.initializeApp(applicationContext)
     }
 }
